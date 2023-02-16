ad89848c3fc40d8bf75b3e3f6f227a36c5dcb4ec
BanziM <bandisilemazomba123@gmail.com>
2023-02-16 15:54:05 -0800

    use puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char str[] = "programming is like building a multilingual puzzle\n";
+
+	puts(str);
+
+	return (0);
+}
